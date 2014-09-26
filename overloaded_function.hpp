011931bd028a69fb1a9455b6a0d5b812587dbb61
pandaxcldev <pandaxcldev@163.com>
2014-09-26 16:28:28 +0800

    added overloaded_functiom template c++ code
    Use overloaded_function template to generate overloaded lambda
    function! If you use it with generic lambda (C++14), then you can
    simulate to use local template code in C++. It is very useful without
    local template. The TEST code is also enclosed in file
    overloaded_function.hpp.

---
+++ overloaded_function.hpp
@@ -0,0 +1,47 @@
+template<typename F, typename...Etc>
+struct overloaded_function:F, overloaded_function<Etc...>
+{
+	overloaded_function(F const&f, Etc...etc):F(f),overloaded_function<Etc...>(etc...){}
+	using F::operator();
+	using overloaded_function<Etc...>::operator();
+};
+template<typename F> struct overloaded_function<F>:F 
+{ 
+	using F::operator();
+	overloaded_function(F const&f):F(f){}
+};
+
+template<typename...Etc>
+overloaded_function<Etc...> make_overloaded_function(Etc...etc)
+{
+	return overloaded_function<Etc...>(etc...);
+}
+
+#if PANDAXCL_OVERLOADED_FUNCTION_TEST
+// :map \\ :!g++ -DPANDAXCL_OVERLOADED_FUNCTION_TEST=1 -xc++ -std=c++1y %<CR>
+#include <iostream>
+int main(int argc, const char*argv[])
+{
+	{// normal lambda
+		int x = 100;
+		auto f = make_overloaded_function(
+		[&x](int a){std::cout<<"a = "<<a<<std::endl; x = 123;},
+		[&x](const char* s){std::cout<<"s = "<<s<<std::endl; x *= 2;}
+		);
+		f(1);std::cout<<"x="<<x<<std::endl;
+		f("pandaxcl");std::cout<<"x="<<x<<std::endl;
+	}
+	{// generic lambda
+		int x = 100;
+		auto f = make_overloaded_function(
+		[&x](int a, auto b){std::cout<<"a = "<<a<<std::endl; x = 123;},
+		[&x](const char* s, auto b){
+			b.hello();// notice here: it is an ERROR, but can PASS compile if not used!!!
+			std::cout<<"s = "<<s<<std::endl; x *= 2;
+		});
+		f(1, 0);std::cout<<"x="<<x<<std::endl;
+		//f("pandaxcl", 0);std::cout<<"x="<<x<<std::endl;// not use this call!!!
+	}
+	return 0;
+}
+#endif//PANDAXCL_OVERLOADED_FUNCTION_TEST
