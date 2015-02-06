55148a3929d5d945091a5cd317bfc478c8b60b95
熊 春雷 <pandaxcl@gmail.com>
2015-02-06 20:33:20 +0800

    1. 先备份最基本的东西

---
+++ NSPredicateEditorTest/NSPredicateEditorTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NSPredicateEditorTest
+//
+//  Created by 熊 春雷 on 15/2/6.
+//  Copyright (c) 2015年 pandaxcl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ NSPredicateEditorTest/NSPredicateEditorTestTests/NSPredicateEditorTestTests.m
@@ -0,0 +1,40 @@
+//
+//  NSPredicateEditorTestTests.m
+//  NSPredicateEditorTestTests
+//
+//  Created by 熊 春雷 on 15/2/6.
+//  Copyright (c) 2015年 pandaxcl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface NSPredicateEditorTestTests : XCTestCase
+
+@end
+
+@implementation NSPredicateEditorTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
