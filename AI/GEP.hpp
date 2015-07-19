617c06297f708a0e74911f623e256925114e2799
熊 春雷 <pandaxcl@gmail.com>
2015-07-19 16:22:30 +0800

    1. 重新调整了GEP算法的整体结构 2. 完成了一部分的演化算法 3. 但是还有一个更重要的GEP适应值计算的过程还没有完成，所以这里先进行提交一下

--- AI/GEP.hpp
+++ AI/GEP.hpp
@@ -38,118 +38,228 @@
 //	};
 //}
 
-template<typename F=int> class node;
-template<typename F=int>
-struct with_function
-{
-	F f;
-	bool is_terminal()
-	{
-		return f < 0;
-	}
-	size_t args_count()
-	{
-		return 2;
-	}
-};
 
-template<typename F=int>
-class DNA:public with_function<F>
-{
-	DNA(const node<F>&n) { this->f = n->f; }
-	DNA(const F f) { this->f = f; }
-};
-template<typename F=int,typename Real=double>
-class gene:private std::vector<DNA<F>>
-{
-	typedef std::shared_ptr<node<F>> node_ptr;
-	node_ptr to_tree()
-	{
-		const gene<F>&K = *this;
-		std::queue<node_ptr> Q;
-		size_t p = 0;
-		auto T = node_ptr(new node<F>(K[p]));
-		p++;
-		Q.push_back(T);
-		while(!Q.empty())
-		{
-			auto n = Q.top(); Q.pop_front();
-			if(n->is_terminal())
-				continue;
-
-			size_t a = n->args_count();
-			for(size_t i=0;i<a;i++)
-			{
-				auto m = node_ptr(new node<F>(K[p]));
-				n.children.push_back(m);
-				p++;
-				Q.push_back(m);
-			}
-		}
-		return T;
-	}
-    template<int N_functions, int N_terminals>
-    void mutate(Real probability, int F_T[])
-    {
-        Real p = 0.0;
-        for (int i=0;i<this->size();i++)
-        {
-            p = static_cast<Real>(std::rand())/RAND_MAX;
-            if (p <= probability)
-            {
-
-            }
-        }
-    }
-};
-
-template<typename F/*=int*/>
-class node:public with_function<F>
-{
-	std::weak_ptr<node<F>> parent;
-	std::vector<std::shared_ptr<node<F>>> children;
-
-public:
-	node(const DNA<F>&n) { this->f = n->f; }
-	node(const F f) { this->f = f; }
-	//std::result_of<F(decltype(children))>::type operator()
-	//{
-	//	return f(children);
-	//}
-
-	gene<F> to_k_expression()
-	{
-		std::queue<node<F>*> Q;
-		gene<F> K;
-		Q.push_back(this);
-		while(!Q.empty())
-		{
-			node<F>*n = Q.top();Q.pop_front();
-			K.push_back(DNA<F>(*n));
-			if(n->is_terminal())
-				continue;
-			for(auto&m:children)
-				Q.push_back(m);
-		}
-
-		return K;
-	}
-};
-
-template<typename F=int>
-class chromosome:private std::vector<gene<F>>
-{
-};
-
-
-template<int N_units, typename Unit, int N_functions, int N_terminals, typename Real=double>
+template<
+    int N_units,
+    int N_genes,
+    int N_headers,
+    int N_maxops,
+    int N_functions,
+    int N_terminals,
+    typename Real=double,
+    typename F_encode=int
+>
 struct gene_experssion_program
 {
+    class node;
+    struct with_function
+    {
+        F_encode f;
+        bool is_terminal()
+        {
+            return f < 0;
+        }
+        size_t args_count()
+        {
+            return 2;
+        }
+    };
+    
+    struct DNA:public with_function
+    {
+        DNA(){ this->f = 0; }
+        DNA(const node&n) { this->f = n->f; }
+        DNA(const F_encode f) { this->f = f; }
+    };
+    struct gene
+    {
+        static const int N_tails = N_headers*(N_maxops-1)+1;
+        static const int N_DNAs = N_headers+N_tails;
+        DNA DNAs[N_DNAs];
+        typedef std::shared_ptr<node> node_ptr;
+        
+        node_ptr to_tree()
+        {
+            const gene&K = *this;
+            std::queue<node_ptr> Q;
+            size_t p = 0;
+            auto T = node_ptr(new node(K[p]));
+            p++;
+            Q.push_back(T);
+            while(!Q.empty())
+            {
+                auto n = Q.top(); Q.pop_front();
+                if(n->is_terminal())
+                    continue;
+                
+                size_t a = n->args_count();
+                for(size_t i=0;i<a;i++)
+                {
+                    auto m = node_ptr(new node(K[p]));
+                    n.children.push_back(m);
+                    p++;
+                    Q.push_back(m);
+                }
+            }
+            return T;
+        }
+    public:
+        void evolve_mutate(Real probability, int F_T[])
+        {
+            Real p = 0.0;
+            for (int i=0;i<N_DNAs;i++)
+            {
+                p = static_cast<Real>(std::rand())/static_cast<Real>(RAND_MAX);
+                if (p <= probability)
+                {
+                    if (i<N_headers)// 可以选择所有的符号
+                        DNAs[i] = F_T[std::rand()%(N_functions+N_terminals)];
+                    else// 只能选择终结符
+                        DNAs[i] = F_T[N_functions+std::rand()%N_terminals];
+                }
+            }
+        }
+        void evolve_reverse(Real probability) // 倒串
+        {
+            Real p = static_cast<Real>(std::rand())/static_cast<Real>(RAND_MAX);
+            if (p <= probability)
+            {
+                int nStart = std::rand()%N_headers;// 倒串只作用到基因的头部
+                int nLength = std::rand()%(N_headers-nStart-2) + 2;// 至少有两个DNA，才谈得上倒串
+                
+                for (int i=nStart; i<nStart+nLength; i++)
+                {
+                    // 12 345 678
+                    // nStart = 2;
+                    // nLength = 3;
+                    // i=2, 2+3-i+1=4
+                    // i=3, 2+3-i+1=3
+                    // i=4, 2+3-i+1=2
+                    std::swap(DNAs[i], DNAs[nStart+nLength-i+1]);
+                }
+            }
+        }
+        
+        void evolve_insert_string(Real probability)// 插串
+        {
+            Real p = static_cast<Real>(std::rand())/static_cast<Real>(RAND_MAX);
+            if (p <= probability)
+            {
+                int nStart = std::rand()%(N_headers+N_tails);
+                int nLength = std::rand()%(N_headers+N_tails-1)+1;// 至少得有1个基因，才能执行插串
+                int nInsert = std::rand()%(N_headers-1)+1; // 因为不能插到基因的第一个位置
+                for (int i=nInsert; i<nInsert+nLength && i+nLength<N_headers; i++)// 插入位置的nLength长度的DNA后移，超出头部长度的直接丢弃
+                {
+                    // 01234 56 7
+                    // 12345 67 8
+                    // nStart = 2
+                    // nLength = 2
+                    // nInsert = 3
+                    
+                    DNAs[i+nLength] = DNAs[i];
+                }
+                for (int i=nInsert; i<nInsert+nLength; i++)
+                {
+                    DNAs[i] = DNAs[nStart+i-nInsert];
+                }
+            }
+        }
+        
+        void evolve_root_insert_string(Real probability)// 根插串
+        {
+            Real p = static_cast<Real>(std::rand())/static_cast<Real>(RAND_MAX);
+            if (p <= probability)
+            {
+                int nStart = std::rand()%(N_headers+N_tails);
+                int nLength = std::rand()%(N_headers+N_tails);
+                int nInsert = 0; // 根插串就是将插串插到基因的第一个位置
+            }
+        }
+        
+        void evolve_single_crossover(Real probability, gene&another)
+        {
+            
+        }
+        
+        void evolve_double_crossover(Real probability, gene&another)
+        {
+            
+        }
+    };
+    
+    class chromosome
+    {
+        gene genes[N_genes];
+        void evolve_mutate(Real probability, int F_T[])
+        {
+            for(auto&g:genes)
+                g.evolve_mutate(probability, F_T);
+        }
+        void evolve_reverse(Real probability) // 倒串
+        {
+            for(auto&g:genes)
+                g.evolve_reverse(probability);
+        }
+        void evolve_insert_string(Real probability)// 插串
+        {
+            
+        }
+        void evolve_root_insert_string(Real probability)// 根插串
+        {
+            
+        }
+        
+        void evolve_single_crossover(Real probability, chromosome&another)
+        {
+            
+        }
+        
+        void evolve_double_crossover(Real probability, chromosome&another)
+        {
+            
+        }
+        
+        void evolve_gene_crossover(Real probability, chromosome&another)
+        {
+            
+        }
+    };
+    
+    class node:public with_function
+    {
+        std::weak_ptr<node> parent;
+        std::vector<std::shared_ptr<node>> children;
+        
+    public:
+        node(const DNA&n) { this->f = n->f; }
+        node(const F_encode f) { this->f = f; }
+        
+        gene to_k_expression()
+        {
+            std::queue<node*> Q;
+            gene K;
+            Q.push_back(this);
+            while(!Q.empty())
+            {
+                node*n = Q.top();Q.pop_front();
+                K.push_back(DNA(*n));
+                if(n->is_terminal())
+                    continue;
+                for(auto&m:children)
+                    Q.push_back(m);
+            }
+            
+            return K;
+        }
+    };
+    
+    typedef typename std::conditional<1 == N_genes, gene, chromosome>::type Unit;
+    
     std::function<Real(const Unit&)> lambda_fitness;// 适应度函数
     std::function<void()> inner_lambda_fitness_standard; // 标准适应度计算
     std::function<void()> inner_lambda_selection_roulette_wheel; // 轮盘赌
+    std::function<void(Real probability)> inner_lambda_mutation_standard;// 标准变异过程
     
-//    template<int N_args, int N_result>
     gene_experssion_program()
     {
         struct Local
@@ -215,6 +325,15 @@ struct gene_experssion_program
             
             local->units = nextGeneration;
         };
+        
+        inner_lambda_mutation_standard = [local](Real probability)
+        {
+            for(int i=0; i<N_units; i++)
+            {
+                Unit&unit = local->units[i];
+                unit.evolve_mutate(probability, local->F_T);
+            }
+        };
     }
 };
 
@@ -226,7 +345,15 @@ g++ -std=c++11 -DTEST_WITH_MAIN_FOR_GEP_HPP=1 -x c++ %
 
 int main(int argc, const char*argv[])
 {
-    auto GEP = gene_experssion_program<100, chromosome<>, 10, 10>();
+    auto GEP = gene_experssion_program<
+    100/*int N_units*/,
+    1/*int N_genes*/,
+    8/*int N_headers*/,
+    2/*int N_maxops*/,
+    4/*int N_functions*/,
+    6/*int N_terminals*/>();
+    
+    
 	return 0;
 }
 
