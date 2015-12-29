496b00b38938724cf79575598e8e5355419a8e69
熊 春雷 <pandaxcl@gmail.com>
2015-12-29 10:54:56 +0800

    1. 对OpenGL的Uniform Block Object进行了初步的封装，这样使用起来就更加简单了；）

--- opengl/glsl4_book.cpp
+++ opengl/glsl4_book.cpp
@@ -112,6 +112,65 @@ namespace Description
 	};
 }// namespace Description
 
+namespace block
+{
+    struct uniform_description
+    {
+    //protected:
+        GLuint      indices[4] = {0};
+        GLint       offset [4] = {0};
+        GLuint      blockIndex = -1;
+        GLint       blockSize  = 0;
+        GLuint      uboHandle  = -1;
+    };
+    template<typename Block>struct uniform:public Block
+    {
+        static_assert(std::is_base_of<uniform_description, Block>::value, "");
+        static_assert(!std::is_void<decltype(Block::N_fields)>::value, "");
+        static_assert(!std::is_void<decltype(Block::name)>::value, "");
+        static_assert(!std::is_void<decltype(Block::names)>::value, "");
+        static_assert( std::is_void<decltype(std::declval<Block>().initialize())>::value, "");
+        static_assert( std::is_void<decltype(std::declval<Block>().update())>::value, "");
+        
+
+        void send_to_opengl(GLuint programHandle)
+        {
+            uniform_description::blockIndex = glGetUniformBlockIndex(programHandle, Block::name);
+            glGetActiveUniformBlockiv(programHandle, uniform_description::blockIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &this->uniform_description::blockSize);
+            glGetUniformIndices(programHandle, Block::N_fields, Block::names, uniform_description::indices);
+            glGetActiveUniformsiv(programHandle, Block::N_fields, uniform_description::indices, GL_UNIFORM_OFFSET, uniform_description::offset);
+            
+            glGenBuffers(1, &this->uniform_description::uboHandle);
+            glBindBuffer(GL_UNIFORM_BUFFER, uniform_description::uboHandle);
+
+            Block::initialize();
+            
+            glBindBufferBase(GL_UNIFORM_BUFFER, uniform_description::blockIndex, uniform_description::uboHandle);
+        }
+//        void initialize()
+//        {
+//            
+//        }
+//        void update()
+//        {
+//            GLubyte * blockBuffer = (GLubyte *)malloc(blockSize);
+//            GLfloat outerColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+//            GLfloat innerColor[] = { 1.0f, 1.0f, 0.75f, 1.0f };
+//            GLfloat innerRadius = 0.25f;
+//            GLfloat outerRadius = 0.45f;
+//            memcpy(blockBuffer + offset[0], innerColor, 4 * sizeof(GLfloat));
+//            memcpy(blockBuffer + offset[1], outerColor, 4 * sizeof(GLfloat));
+//            memcpy(blockBuffer + offset[2], &innerRadius, sizeof(GLfloat));
+//            memcpy(blockBuffer + offset[3], &outerRadius, sizeof(GLfloat));
+//            
+//            glBindBuffer(GL_UNIFORM_BUFFER, uboHandle);
+//            glBufferData(GL_UNIFORM_BUFFER, blockSize, blockBuffer, GL_DYNAMIC_DRAW);
+//            
+//            free(blockBuffer);
+//        }
+    };
+}
+
 TEST_CASE("第一个例子", "[GLSL4BOOK]")
 {
 	struct Local
@@ -235,104 +294,104 @@ void main()
 
 TEST_CASE("通过uniform块变量传递数据到片元着色器", "[GLSL4BOOK][active]")
 {
-	struct Local
-	{
-		vertex_array_object<Description::Rectangle> vao;
-		GLuint programHandle = 0;
-	};
-	auto local = std::make_shared<Local>();
+    struct BlobSettings:block::uniform_description
+    {
+        enum{  N_fields = 4 };
+        const GLchar*name = "BlobSettings";
+        const GLchar *names[N_fields] = {
+            "InnerColor",
+            "OuterColor",
+            "RadiusInner",
+            "RadiusOuter",
+        };
+        void initialize()
+        {
 
-	opengl render;
-	render
-		.initialize([local, &render]() {
-			{
-				struct Description
-				{
-					static const GLchar*vertex_shader()
-					{
-						return u8R"(
-#version 400 
-layout (location = 0) in vec3 VertexPosition; 
-layout (location = 1) in vec3 VertexTexCoord; 
-out vec3 TexCoord; 
-void main() 
-{    
-	TexCoord = VertexTexCoord;    
-	gl_Position = vec4(VertexPosition,1.0); 
-} 
-                       )";
-					}
-					static const GLchar*fragment_shader()
-					{
-						return u8R"(
-#version 400 
-in vec3 TexCoord; 
-layout (location = 0) out vec4 FragColor;
-uniform BlobSettings 
-{  
-    vec4 InnerColor;  
-    vec4 OuterColor;  
-    float RadiusInner;  
-    float RadiusOuter; 
-}; 
-void main() 
-{    
-    float dx = TexCoord.x - 0.5;    
-    float dy = TexCoord.y - 0.5;    
-    float dist = sqrt(dx * dx + dy * dy);    
-    FragColor = mix( InnerColor, OuterColor, smoothstep( RadiusInner, RadiusOuter, dist ) ); 
-} 
-                       )";
-					}
-				};
-				local->programHandle = gpu_program<Description>().send_to_opengl().report().gl_handle();
-			}
-			{
-				local->vao.send_to_opengl();
-			}
-	})
-		.display([local]() {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(local->programHandle);
+        }
+        void update()
+        {
+            GLubyte * blockBuffer = (GLubyte *)malloc(blockSize);
+            GLfloat outerColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+            GLfloat innerColor[] = { 1.0f, 1.0f, 0.75f, 1.0f };
+            GLfloat innerRadius = 0.25f;
+            GLfloat outerRadius = 0.45f;
+            memcpy(blockBuffer + offset[0], innerColor, 4 * sizeof(GLfloat));
+            memcpy(blockBuffer + offset[1], outerColor, 4 * sizeof(GLfloat));
+            memcpy(blockBuffer + offset[2], &innerRadius, sizeof(GLfloat));
+            memcpy(blockBuffer + offset[3], &outerRadius, sizeof(GLfloat));
 
-		{
-			GLuint programHandle = local->programHandle;
-			GLuint blockIndex = glGetUniformBlockIndex(programHandle, "BlobSettings");
-			GLint blockSize;
-			glGetActiveUniformBlockiv(programHandle, blockIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
-			// Query for the offsets of each block variable 
-			const GLchar *names[] = {
-				"InnerColor",
-				"OuterColor",
-				"RadiusInner",
-				"RadiusOuter"
-			};
-			GLuint indices[4];
-			glGetUniformIndices(programHandle, 4, names, indices);
-			GLint offset[4];
-			glGetActiveUniformsiv(programHandle, 4, indices, GL_UNIFORM_OFFSET, offset);
-
-			GLubyte * blockBuffer = (GLubyte *)malloc(blockSize);
-			GLfloat outerColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
-			GLfloat innerColor[] = { 1.0f, 1.0f, 0.75f, 1.0f };
-			GLfloat innerRadius = 0.25f;
-			GLfloat outerRadius = 0.45f;
-			memcpy(blockBuffer + offset[0], innerColor, 4 * sizeof(GLfloat));
-			memcpy(blockBuffer + offset[1], outerColor, 4 * sizeof(GLfloat));
-			memcpy(blockBuffer + offset[2], &innerRadius, sizeof(GLfloat));
-			memcpy(blockBuffer + offset[3], &outerRadius, sizeof(GLfloat));
-
-			GLuint uboHandle;
-			glGenBuffers(1, &uboHandle);
-			glBindBuffer(GL_UNIFORM_BUFFER, uboHandle);
-			glBufferData(GL_UNIFORM_BUFFER, blockSize, blockBuffer, GL_DYNAMIC_DRAW);
-
-			free(blockBuffer);
-
-			glBindBufferBase(GL_UNIFORM_BUFFER, blockIndex, uboHandle);
-		}
-		local->vao.display();
-	});
+            glBindBuffer(GL_UNIFORM_BUFFER, uboHandle);
+            glBufferData(GL_UNIFORM_BUFFER, blockSize, blockBuffer, GL_DYNAMIC_DRAW);
+            
+            free(blockBuffer);
+        }
+    };
+    struct Local
+    {
+        vertex_array_object<Description::Rectangle> vao;
+        block::uniform<BlobSettings> blobSettings;
+        GLuint programHandle = 0;
+    };
+    auto local = std::make_shared<Local>();
+    
+    opengl render;
+    render
+    .initialize([local, &render]() {
+        {
+            struct Description
+            {
+                static const GLchar*vertex_shader()
+                {
+                    return u8R"(
+#version 400
+                    layout (location = 0) in vec3 VertexPosition;
+                    layout (location = 1) in vec3 VertexTexCoord;
+                    out vec3 TexCoord;
+                    void main()
+                    {
+                        TexCoord = VertexTexCoord;
+                        gl_Position = vec4(VertexPosition,1.0);
+                    }
+                    )";
+                }
+                static const GLchar*fragment_shader()
+                {
+                    return u8R"(
+#version 400
+                    in vec3 TexCoord;
+                    layout (location = 0) out vec4 FragColor;
+                    uniform BlobSettings
+                    {
+                        vec4 InnerColor;
+                        vec4 OuterColor;
+                        float RadiusInner;
+                        float RadiusOuter;
+                    };
+                    void main()
+                    {
+                        float dx = TexCoord.x - 0.5;
+                        float dy = TexCoord.y - 0.5;
+                        float dist = sqrt(dx * dx + dy * dy);
+                        FragColor = mix( InnerColor, OuterColor, smoothstep( RadiusInner, RadiusOuter, dist ) ); 
+                    } 
+                    )";
+                }
+            };
+            local->programHandle = gpu_program<Description>().send_to_opengl().report().gl_handle();
+        }
+        {
+            local->vao.send_to_opengl();
+        }
+        {
+            local->blobSettings.send_to_opengl(local->programHandle);
+        }
+    })
+    .display([local]() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(local->programHandle);
+        local->blobSettings.update();
+        local->vao.display();
+    });
 
 	window() << render;
 }
