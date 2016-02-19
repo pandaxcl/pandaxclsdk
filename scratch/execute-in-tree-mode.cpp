2217dea913e02cf6f64bb429f756d1b801b204cc
熊 春雷 <pandaxcl@gmail.com>
2016-02-19 18:50:54 +0800

    1. 勉强完成了树形代码的期望执行的第一步：）但是执行所有的测试路径尚未成功：（

--- scratch/execute-in-tree-mode.cpp
+++ scratch/execute-in-tree-mode.cpp
@@ -1,56 +1,65 @@
 #include <iostream>
 #include <vector>
-
-template<long>
-struct ShouldExecute
+struct flag
 {
-	static int flag;
-	static bool OK() { return flag > 0; }
+	int _count;
+	explicit flag(int count):_count(count) { std::cout<<"构造"<<std::endl;}
+	~flag() { _count --; std::cout<<"析构"<<std::endl; if(0==_count) throw; }
+	operator bool() 
+	{ 
+		std::cout<< ". count = " << _count << std::endl;
+		return _count>0; 
+	}
 };
-template<long N>
-int ShouldExecute<N>::flag = 0;
 
 int main(int argc, const char*argv[])
 {
-#define CASE(comment)\
-	if (isBuild) { *p++; } else { *p--; } \
-	p = &ShouldExecute<__LINE__>::flag; \
-	if(ShouldExecute<__LINE__>::OK())
 
-	int*p = &ShouldExecute<__LINE__>::flag;
-	auto f = [&p](bool isBuild)
+#define INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line ) name##line
+#define INTERNAL_CATCH_UNIQUE_NAME_LINE( name, line ) INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line )
+#define INTERNAL_CATCH_UNIQUE_NAME( name ) INTERNAL_CATCH_UNIQUE_NAME_LINE( name, __LINE__ )
+//#define CAT(name) INTERNAL_CATCH_UNIQUE_NAME(name)
+#define CAT(name) INTERNAL_CATCH_UNIQUE_NAME_LINE(name, __LINE__)
+
+#define CASE(n)\
+	static flag CAT(v)(n);\
+	if(auto CAT(vv)=CAT(v))
+
+	auto f = []()
 	{
-		CASE("1"){
+		CASE(3){
 			std::cout<<__LINE__<<std::endl;
-			CASE("1,1"){
+			CASE(1){
 				std::cout<<__LINE__<<std::endl;
 			}
-			CASE("1,2"){
+			CASE(1){
 				std::cout<<__LINE__<<std::endl;
 			}
-			CASE("1,3"){
+			CASE(1){
 				std::cout<<__LINE__<<std::endl;
 			}
 		}
-		CASE("2"){
+		CASE(1){
 			std::cout<<__LINE__<<std::endl;
 		}
-		CASE("3"){
+		CASE(2){
 			std::cout<<__LINE__<<std::endl;
-			CASE("3,1"){
+			CASE(1){
 				std::cout<<__LINE__<<std::endl;
 			}
-			CASE("3,2"){
+			CASE(1){
 				std::cout<<__LINE__<<std::endl;
 			}
 		}
-		CASE("4"){
+		CASE(1){
 			std::cout<<__LINE__<<std::endl;
 		}
 	};
-	f(true);
-	std::cout<<"==========="<<std::endl;
-	f(false);
+
+	while(true)
+	{
+		try { f(); break; } catch(...) {}
+	}
 
 	return 0;
 }
