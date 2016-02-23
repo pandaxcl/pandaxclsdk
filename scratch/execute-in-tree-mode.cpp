656e3698afe8175182bf2732f1352e3e8fa415da
熊 春雷 <pandaxcl@gmail.com>
2016-02-23 11:23:26 +0800

    1. 第一个可用的能够按照tree模式执行的代码完成了；）

--- scratch/execute-in-tree-mode.cpp
+++ scratch/execute-in-tree-mode.cpp
@@ -1,65 +1,78 @@
 #include <iostream>
-#include <vector>
-struct flag
+#include <set>
+
+class should_execute_t
 {
-	int _count;
-	explicit flag(int count):_count(count) { std::cout<<"构造"<<std::endl;}
-	~flag() { _count --; std::cout<<"析构"<<std::endl; if(0==_count) throw; }
-	operator bool() 
-	{ 
-		std::cout<< ". count = " << _count << std::endl;
-		return _count>0; 
+	std::set<int> flags;
+public:
+	bool yes(int line)    { return flags.end() == flags.find(line); }
+	void record(int line) { flags.insert(line); }
+	void clear()          { flags.clear(); }
+};
+template<int LINE> struct section
+{
+	should_execute_t &_should_execute;
+	int              &_max_depth_line;
+	section(should_execute_t&should_execute, int&max_depth_line)
+	: _should_execute(should_execute)
+	, _max_depth_line(max_depth_line)
+	{
+		if (_should_execute.yes(LINE))
+			_max_depth_line = LINE;
+	}
+	~section() 
+	{
+		if (LINE == _max_depth_line)
+			_should_execute.record(LINE);
+	}
+	operator bool()
+	{
+		return _should_execute.yes(LINE);
 	}
 };
 
-int main(int argc, const char*argv[])
+#define SECTION()\
+	if (auto&&v=section<__LINE__>(should_execute, max_depth_line))
+
+#define TEST_CASE(reenter)                 \
+	static should_execute_t should_execute;\
+	if (reenter) should_execute.clear();   \
+	int max_depth_line = 0;                \
+	SECTION()
+
+
+void f(bool reenter=false)
 {
-
-#define INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line ) name##line
-#define INTERNAL_CATCH_UNIQUE_NAME_LINE( name, line ) INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line )
-#define INTERNAL_CATCH_UNIQUE_NAME( name ) INTERNAL_CATCH_UNIQUE_NAME_LINE( name, __LINE__ )
-//#define CAT(name) INTERNAL_CATCH_UNIQUE_NAME(name)
-#define CAT(name) INTERNAL_CATCH_UNIQUE_NAME_LINE(name, __LINE__)
-
-#define CASE(n)\
-	static flag CAT(v)(n);\
-	if(auto CAT(vv)=CAT(v))
-
-	auto f = []()
+	std::cout<<"1"<<std::endl;
+	TEST_CASE(reenter)
 	{
-		CASE(3){
-			std::cout<<__LINE__<<std::endl;
-			CASE(1){
-				std::cout<<__LINE__<<std::endl;
-			}
-			CASE(1){
-				std::cout<<__LINE__<<std::endl;
-			}
-			CASE(1){
-				std::cout<<__LINE__<<std::endl;
-			}
+		std::cout<<"2"<<std::endl;
+		SECTION() // 子代码段1
+		{
+			std::cout<<"3"<<std::endl;
+			return;// 比较讨厌
 		}
-		CASE(1){
-			std::cout<<__LINE__<<std::endl;
+		SECTION() // 子代码段2
+		{
+			std::cout<<"4"<<std::endl;
+			return;// 比较讨厌
 		}
-		CASE(2){
-			std::cout<<__LINE__<<std::endl;
-			CASE(1){
-				std::cout<<__LINE__<<std::endl;
-			}
-			CASE(1){
-				std::cout<<__LINE__<<std::endl;
-			}
-		}
-		CASE(1){
-			std::cout<<__LINE__<<std::endl;
-		}
-	};
-
-	while(true)
-	{
-		try { f(); break; } catch(...) {}
+		return;// 比较讨厌
 	}
-
+}
+int main()
+{
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"==========="<<std::endl;
+	f(true);std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"-----------"<<std::endl;
+	f();std::cout<<"==========="<<std::endl;
 	return 0;
 }
