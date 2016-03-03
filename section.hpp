f5c9f0372b72e1a284ac0af4ce870dbe1eedf548
熊 春雷 <pandaxcl@gmail.com>
2016-03-03 10:18:54 +0800

    1. 将section.hpp分支添加到了即将公用的pandaxclsdk库里面

---
+++ section.hpp
@@ -0,0 +1,6 @@
+#ifndef __PANDAXCLSDK_GIT_SECTION_HPP__
+#define __PANDAXCLSDK_GIT_SECTION_HPP__
+namespace xcl {
+#include "./src/pandaxclsdk.git/section.hpp/section/section.hpp"
+} //namespace xcl {
+#endif//__PANDAXCLSDK_GIT_SECTION_HPP__
