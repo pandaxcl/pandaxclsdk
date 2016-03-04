281b35eaa1225638164acb297453ea8062c80493
熊 春雷 <pandaxcl@gmail.com>
2016-03-04 19:49:29 +0800

    1. 进行section.hpp的头文件重整

--- section.hpp
+++ section.hpp
@@ -1,5 +1,6 @@
 #ifndef __PANDAXCLSDK_GIT_SECTION_HPP__
 #define __PANDAXCLSDK_GIT_SECTION_HPP__
+#include <unordered_set>
 namespace pandaxcl {
 #include "./src/pandaxclsdk.git/section.hpp/section/section.hpp"
 } //namespace pandaxcl {
